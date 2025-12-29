3760e74eccee2d02e1cf00f6e6c36878b9dc1b77
Kevin Lactio Kemta <kevinlactiokemta@gmail.com>
2025-12-29 20:11:56 +0100

    optimizing seo referencing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 
 import tailwindcss from '@tailwindcss/vite';
@@ -17,7 +18,7 @@ export default defineConfig({
     format: 'directory'
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
